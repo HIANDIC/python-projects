636bc1b94e49124694e3acbe9d6cc3744ec6a7a7
HIANDIC <leaands25@gmail.com>
2022-09-26 13:54:47 +0200

    some explanations are added

--- findingMaxNumber.py
+++ findingMaxNumber.py
@@ -2,6 +2,7 @@
 numbers = []
 
 # for loop defined for taking 5 numbers from user
+# here for loop is between 1-5. Therefore it is fit for taking number from user in a user frendly way
 for i in range(1, 6):
     number = int(input(f'Enter {i}. number : '))
     numbers.append(number)
@@ -10,6 +11,7 @@ for i in range(1, 6):
 # first element assigned as a maximum number for comparison
 max_number = numbers[0]
 
+# here for lopp is between 0-4. Therefore it is compatible with the index number
 for i in range(5):
     if numbers[i] > max_number:
         max_number = numbers[i]
