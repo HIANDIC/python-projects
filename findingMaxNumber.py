233242f26360ba5514050524300bd139db5d893e
HIANDIC <leaands25@gmail.com>
2022-09-26 13:54:03 +0200

    some explanations are added

--- findingMaxNumber.py
+++ findingMaxNumber.py
@@ -7,9 +7,15 @@ for i in range(1, 6):
     numbers.append(number)
 
 # finding max number in the created list
+# first element assigned as a maximum number for comparison
+max_number = numbers[0]
 
+for i in range(5):
+    if numbers[i] > max_number:
+        max_number = numbers[i]
 
 # for debuging the list
-print(numbers)
+# print(numbers)
 
 # displays the result
+print(f'Maximum number of the given list is {max_number} !!!')
