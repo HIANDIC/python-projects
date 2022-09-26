f8a2d161eb4691106a313fd8eeffe9b924710639
HIANDIC <leaands25@gmail.com>
2022-09-26 13:46:54 +0200

    for loop for creating a list with the given numbers from user

--- findingMaxNumber.py
+++ findingMaxNumber.py
@@ -1,11 +1,15 @@
 # creating an empty list
+numbers = []
 
 # for loop defined for taking 5 numbers from user
-
+for i in range(1, 6):
+    number = int(input(f'Enter {i}. number : '))
+    numbers.append(number)
 
 # finding max number in the created list
 
 
 # for debuging the list
+print(numbers)
 
 # displays the result
